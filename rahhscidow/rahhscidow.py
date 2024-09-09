7e4e87d062735459ed9cb7f8f0c843d193d7e1bd
toloknieiev <82337668+toloknieiev@users.noreply.github.com>
2024-09-09 03:38:20 +0200

    Update rahhscidow.py

--- rahhscidow/rahhscidow.py
+++ rahhscidow/rahhscidow.py
@@ -22,51 +22,53 @@ def call_module(module_name, args=None):
     subprocess.run(command)
 
 
-print("""
-           
-                                                 
+def main():
+    print("""
+               
+                                                     
+    
+    
+               _     _              _     _               
+              | |   | |            (_)   | |              
+     _ __ __ _| |__ | |__  ___  ___ _  __| | _____      __
+    | '__/ _` | '_ \| '_ \/ __|/ __| |/ _` |/ _ \ \ /\ / /
+    | | | (_| | | | | | | \__ \ (__| | (_| | (_) \ V  V / 
+    |_|  \__,_|_| |_|_| |_|___/\___|_|\__,_|\___/ \_/\_/  
+                                                          
+                                                          
+    
+                                                    
+    
+    
+    """)
+    file_path = input("Yield DOI list: ") 
+    target_path = input("Yield target directory: ")
+    scihub_mirror = input("Yield scihub mirror in https://[link_name] format or leave empty for default: ").strip()
+    if not scihub_mirror:
+        scihub_mirror = "https://www.sci-hub.wf/"
+    strings_list = read_strings_from_file(file_path)
+    
+    
+    spec = importlib.util.find_spec('sci-hub')
+    if spec is not None:
+        print('Dependencies OK.')
+    else:
+        call_module("pip", ["install", "sci-hub"])
+    
+    
+    
+    for element in strings_list:
+        if element == "":
+            continue
+        try:
+            call_module("scihub", ['-s', f'{element}', "-O", f"{target_path}", "-u", f"{scihub_mirror}"])
+        except Exception:
+            print(f"Retrieval of {element} failed, trying next element...")
+    
+    print("Paper retrieval done")
 
-
-           _     _              _     _               
-          | |   | |            (_)   | |              
- _ __ __ _| |__ | |__  ___  ___ _  __| | _____      __
-| '__/ _` | '_ \| '_ \/ __|/ __| |/ _` |/ _ \ \ /\ / /
-| | | (_| | | | | | | \__ \ (__| | (_| | (_) \ V  V / 
-|_|  \__,_|_| |_|_| |_|___/\___|_|\__,_|\___/ \_/\_/  
-                                                      
-                                                      
-
-                                                
-
-
-""")
-file_path = input("Yield DOI list: ") 
-target_path = input("Yield target directory: ")
-scihub_mirror = input("Yield scihub mirror in https://[link_name] format or leave empty for default: ").strip()
-if not scihub_mirror:
-    scihub_mirror = "https://www.sci-hub.wf/"
-strings_list = read_strings_from_file(file_path)
-
-
-spec = importlib.util.find_spec('sci-hub')
-if spec is not None:
-    print('Dependencies OK.')
-else:
-    call_module("pip", ["install", "sci-hub"])
-
-
-
-for element in strings_list:
-    if element == "":
-        continue
-    try:
-        call_module("scihub", ['-s', f'{element}', "-O", f"{target_path}", "-u", f"{scihub_mirror}"])
-    except Exception:
-        print(f"Retrieval of {element} failed, trying next element...")
-
-print("Paper retrieval done")
-
-
+if __name__ == "__main__":
+    main()
 
 
 
