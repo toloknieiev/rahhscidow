e17904e4f8e46b792cca5cfce78d16eb31fb0e7a
toloknieiev <82337668+toloknieiev@users.noreply.github.com>
2024-09-09 03:09:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
+import pathlib
+
+# Get the long description from the README file
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
-    name='rahhscidow',
+    name='rahhscidow',  # Your package name
     version='1.0',
-    author='Artur Toloknieiev',
-    author_email='toloknieiev.artur@gmail.com',
-    description='Random Ahh Sci-Hub Downloader. For when you really need to download a bunch of papers.',
+    description='A Scihub paper downloader.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Use 'text/markdown' if you're using Markdown
     packages=find_packages(),
     python_requires='>=3.6',
+    install_requires=[],  # Add your dependencies here
+    # other setup parameters
 )
